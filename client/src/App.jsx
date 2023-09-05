be5a48090d97cad7ad91679c5636b0ea849bbd1f
Juan Santos <juansantos@mail.weber.edu>
2023-09-05 17:48:46 -0600

    Saving test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import Hello from './components/HelloWorld'
+import "./App.css";
+import Hello from "./components/HelloWorld";
 
 function App() {
-
   return (
     <>
       <h1>At least 4</h1>
       <Hello />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
