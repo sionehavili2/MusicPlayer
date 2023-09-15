c09cc257de905d2f4ca9684d1edf94f4bd5d3404
Juan Santos <juansantos@mail.weber.edu>
2023-09-14 20:43:25 -0600

    Adding Functionality for Login and Registration

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavigationBar from "../components/Navigation";
+import Hello from "../components/HelloWorld";
+
+function HomePage() {
+  return (
+    <div>
+        <NavigationBar />
+      <h1>At least 4</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default HomePage;
--- server/server.mjs
+++ server/server.mjs
@@ -2,7 +2,6 @@ import express, { response } from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import db from "./db/conn.mjs";
-//import http from "http";
 
 const app = express();
 app.use(express.json());
@@ -15,12 +14,73 @@ app.use(
 );
 
 const PORT = process.env.PORT || 4000;
+// DO NOT EDIT ABOVE THIS LINE
 
-//GET Requests
+// HELPER FUNCTIONS
+function saltShaker(length) {
+  let salt = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    salt += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return salt;
+}
+
+//API SECTION BELOW
+
+// GET REQUESTS
 app.get("/", (req, res) => res.send("Hello, World!"));
-app.get("/helloworld", (req,res) => {
+app.get("/salt", (req, res) => res.send(saltShaker(8)));
+app.get("/helloworld", (req, res) => {
   console.log("Hello World!");
   res.send("Hello World!");
+});
+
+// POST REQUESTS
+app.post("/account", async (req, res) => {
+  let newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    type: req.body.type,
+    salt: req.body.salt,
+  };
+  let collection = await db.collection("users");
+  let result = await collection.insertOne(newUser);
+  res.send(result);
+});
+
+app.post("/loginWithSalt", async (req, res) => {
+  const username = req.body.username;
+
+  let collection = await db.collection("users");
+  let results = await collection.find({ username: username });
+  console.log(results);
+  results.forEach((document) => {
+    let tempSalt = document.salt;
+    res.send(tempSalt);
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  let collection = await db.collection("users");
+  let results = await collection.find({ username: username }).toArray();
+
+  results.forEach((document) => {
+    let storedUsername = document.username;
+    let storedPassword = document.password;
+    if (username === storedUsername && password === storedPassword) {
+      res.send({ loggedIn: true }).status(200);
+    } else {
+      res.send({ loggedIn: false }).status(200);
+    }
+  });
 });
 
 app.listen(PORT, () => {
