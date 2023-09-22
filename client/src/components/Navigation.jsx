1f2aba11220f2872349755d3df3f9c9a4bdfee66
Juan Santos <juansantos@mail.weber.edu>
2023-09-21 19:52:33 -0600

    Adjusted look of Navigation bar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,13 +1,13 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import "bootstrap/dist/css/bootstrap.css";
 
 const NavigationBar = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/">Navigation</Navbar.Brand>
+        <Navbar.Brand href="/">Earbook</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="Login">Login</Nav.Link>
