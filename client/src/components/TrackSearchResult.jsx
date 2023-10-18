6f9a5184ffd73f64e5b20a360d3a90f75c7e83ed
Junsiwoo45 <haydenbackwards@gmail.com>
2023-10-17 20:18:54 -0600

    queue Button updated

--- client/src/components/TrackSearchResult.jsx
+++ client/src/components/TrackSearchResult.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {Button} from "react-bootstrap"
-
+import axios from 'axios';
 export default function TrackSearchResult({ track, chooseTrack, accessToken}) {
   
   function handlePlay() {
@@ -39,7 +39,8 @@ export default function TrackSearchResult({ track, chooseTrack, accessToken}) {
       'Authorization': auth
     };
     // Construct the full API call
-    const fullAPICall = `https://api.spotify.com/v1/me/player/queue/${track.uri}`;
+    const fullAPICall = `https://api.spotify.com/v1/me/player/queue/?uri=${track.uri}`;
+    console.log(fullAPICall)
     axios.post(fullAPICall, data, {headers})
         .then(res => {
            console.log(res);
@@ -61,11 +62,11 @@ export default function TrackSearchResult({ track, chooseTrack, accessToken}) {
       <div className="ml-3">
         <div>{track.title}</div>
         <div className="text-muted">{track.artist}</div>
-        <button onClick={addSongClient}>test button</button>
+        <button onClick={addSongClient}>Add Song to Queue</button>
       </div>
-      <Button className="ml-auto" onClick={handleAddToQueue}>
+      {/* <Button className="ml-auto" onClick={handleAddToQueue}>
         Add to Queue
-      </Button>
+      </Button> */}
     </div>
   )
 }
