c250a5055ac118c7bab0e6b451ec40b3644bb0a5
Tyler Gregory <me@tcgregory.dev>
2023-10-19 19:48:17 -0600

    commuinicating

--- client/src/Pages/NewsApi.jsx
+++ client/src/Pages/NewsApi.jsx
@@ -25,47 +25,46 @@ function NewsApi() {
   // },[]);
 
   useEffect(() => {
+    console.log("HERE!");
     const getPosts = async () => {
       console.log("Requesting posts");
-      const response = await axios.get("http://localhost:4000/posts").then((res) => {
-        console.log(response);
-        setPosts(response.data.posts);
-      });
-      
+      const response = await axios.get("http://localhost:4000/posts");
+      console.log(response.data[0]);
+      // setPosts(response.data.posts); 
     };
     getPosts();
   }, []);
 
 
-  console.log("posts:\n",posts);
+  // console.log("posts:\n",posts);
 
-  return (
-    <div className='App'>
-      <NavigationBar />
-      {news &&
-        news.map((item,index) => {
-          return (
+//   return (
+//     <div className='App'>
+//       <NavigationBar />
+//       {news &&
+//         news.map((item,index) => {
+//           return (
             
             
-            // <Card 
-            // key={index}
-            // hoverable
-            // style={{width: "70%"}}
-            // conver={<img alt="" src={item.urlToImage}/>} 
-            // >
-            //   <Meta title={item.title} description={item.content}/>
-            //   <a href={item.url} target="_blank" rel={"noopener noreferrer"}>
-            //     <Button type="primary" style={{marginTop: "10px"}}>
-            //       Read More
-            //     </Button>
-            //   </a>
-              <button onClick={()=>{sendLikeToServer()}}>Like</button>
-            /* </Card> */
-          );
-        })}
+//             // <Card 
+//             // key={index}
+//             // hoverable
+//             // style={{width: "70%"}}
+//             // conver={<img alt="" src={item.urlToImage}/>} 
+//             // >
+//             //   <Meta title={item.title} description={item.content}/>
+//             //   <a href={item.url} target="_blank" rel={"noopener noreferrer"}>
+//             //     <Button type="primary" style={{marginTop: "10px"}}>
+//             //       Read More
+//             //     </Button>
+//             //   </a>
+//               <button onClick={()=>{sendLikeToServer()}}>Like</button>
+//             /* </Card> */
+//           );
+//         })}
 
-    </div>
-  );
+//     </div>
+//   );
 }
 
 export default NewsApi;
